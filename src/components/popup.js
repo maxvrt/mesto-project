bb615c6b76f2b717836173fbd5d5344ea1256653
Mike Starina <x@pnhd.ru>
2022-05-01 14:43:26 +0300

    popup class added

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,9 +1,31 @@
+import { electron } from "webpack";
+
 export default class Popup {
     constructor(popup) {
         this._popup = popup;
     }
 
+    setEventListeners() {
+        document.addEventListener('keydown', this._handleEscClose);
+        this._popup.addEventListener('click', this.close);
+    }
+
     open() {
-        
+        this.setEventListeners();
+        this._popup.classList.add('popup_opened');
+    }
+
+    close(evt) {
+        if (evt.target.classlist.contains('popup__popup-box') || evt.target.classlist.contains('popup__close')) {
+            this._popup.classList.remove('popup_opened');
+        }
+        document.removeEventListener('keydown', this._handleEscClose);
+    }
+
+    _handleEscClose() {
+        if (evt.key === 'Escape' && this._popup.classList.contains('popup_opened')){
+              this.close();
+        }
+          
     }
 }
